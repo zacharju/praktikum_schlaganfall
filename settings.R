ffbdc053130ecd2a581e24fa436c8aae8c6e5273
Hanan Loulou <H.Loulou@campus.lmu.de>
2025-03-10 23:35:23 +0100

    Weitere Pakete

--- settings.R
+++ settings.R
@@ -1,6 +1,11 @@
-# Hier werden alle globalen Variablen definiert, dateipfade definiert und die pakete geladen
+# This is where all global variables and file paths are defined and the packages are loaded
 
 require("tidyverse")
 require("scales")
+require("rnaturalearth")
+require("rnaturalearthdata")
+require("sf")
+require("viridis")
+require("ggokabeito")
 
 data_path <- "./Daten/Rohdaten/data.CSV"
