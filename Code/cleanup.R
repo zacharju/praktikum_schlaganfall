40f1b63f48c7ffcf2e500ac7bf4d4361196feb5c
Julian Zacharias <j.zacharias@campus.lmu.de>
2025-03-10 11:57:08 +0100

    Hinzufügen einer treatment spalte welche das dem Patienten verabreichte Medikament angibt

--- Code/cleanup.R
+++ Code/cleanup.R
@@ -14,7 +14,7 @@ clean_data <- function(data) {
     )))
   
   #Erstellen der Spalten recurring, recurring.stroke.type und date.of.recurrence
-  cleaned |>
+  cleaned <- cleaned |>
     mutate(
       recurring = ifelse(rowSums(cleaned[, c("DRSISC", "DRSH", "DRSUNK")] == "Y") > 0, "Yes", "No"),
       recurring.stroke.type = ifelse(DRSISC == "Y", "Ischaemic Stroke",
@@ -23,5 +23,15 @@ clean_data <- function(data) {
       date.of.recurrence = ifelse(DRSISC == "Y", DRSISCD,
                                   ifelse(DRSH == "Y", DRSHD,
                                          ifelse(DRSUNK == "Y", DRSUNKD, NA))))
-  
+  cleaned <- cleaned |> 
+    mutate(
+      treatment = case_when(
+        rowSums(cleaned[, c("DASP14", "DLH14", "DMH14", "DSCH", "DIVH")] == "U") > 0 ~ NA_character_, #min ein U in relev. var. -> NA
+        DASP14 == "Y" & (rowSums(cleaned[, c("DLH14", "DMH14", "DHH14", "DSCH", "DIVH")] == "Y") > 0) ~ "Aspirin & Heparin",
+        DASP14 == "Y" ~ "Aspirin",
+        rowSums(cleaned[, c("DLH14", "DMH14", "DHH14", "DSCH", "DIVH")] == "Y") > 0 ~ "Heparin",
+        rowSums(cleaned[, c("DASP14", "DLH14", "DMH14", "DSCH", "DIVH")] == "N") == 5 ~ "No Treatment",
+        TRUE ~ NA_character_ # alle übrigen fälle werden zu NA insbesondere alle spalten bei denen min. eine der relevanten variablen ein "" enthalten werden zu NA 
+      )
+    )
 }
