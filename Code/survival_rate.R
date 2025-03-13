8e344bb2e606881b308a7348b3a5a73bd4b5f94d
Julian Zacharias <j.zacharias@campus.lmu.de>
2025-03-13 19:53:41 +0100

    Anpassen des Themes in survival_rate()

--- Code/survival_rate.R
+++ Code/survival_rate.R
@@ -2,7 +2,7 @@ survival_rate <- function(data = cleaned) {
   data |>
     filter(FDEADC == 2 | FDEADC == 3) |> 
     filter(!is.na(FDEADD)) |> 
-    filter(stroke.type != "Not a Stroke") |>  
+    filter(!is.na(stroke.type)) |>  
     filter(!is.na(treatment)) |> 
     group_by(treatment) |>  
     mutate(
@@ -19,14 +19,18 @@ survival_rate <- function(data = cleaned) {
     ungroup() |>   
   ggplot(aes(x = FDEADD, y = survival_rate, colour = treatment)) +
     geom_line(size = 1) +
-    labs(x = "Tage seit Randomisierung", y = "Überlebensrate") +
+    labs(x = "Tage seit Randomisierung", y = "Überlebensrate", title = "Überlebensrate nach Erstschlaganfall: Einfluss der Therapie") +
     scale_y_continuous(labels = scales :: percent, expand = expansion(mult = c(0, 0.05))) +
     theme_minimal() +
     theme(
-      plot.title = element_text(face = "bold", size = 19, hjust = 0.5),
+      plot.title = element_text(face = "bold", size = 19, hjust = 0.5, lineheight = 1.2),
       axis.title = element_text(face = "bold", size = 15), 
-      axis.text.x = element_text(size = 15),
+      axis.text.x = element_text(size = 14),
       axis.text.y = element_text(size = 14),
+      panel.grid.major.x = element_blank(),  
+      panel.grid.minor.x = element_blank(), 
+      legend.text = element_text(size = 10),
+      legend.title = element_text(size = 12)
     ) +
     scale_color_manual(values = c("#6c00a8", "#c7427c", "#f78212", "#fccd25"))
 }
