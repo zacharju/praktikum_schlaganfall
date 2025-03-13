c140661cef663868e256cef112ee89e6f395c05b
Julian Zacharias <j.zacharias@campus.lmu.de>
2025-03-13 15:00:58 +0100

    Beheben eines Fehlers in survival_rate.R

--- Code/survival_rate.R
+++ Code/survival_rate.R
@@ -16,10 +16,17 @@ survival_rate <- function(data = cleaned) {
       FDEADD_count_cumsum = cumsum(FDEADD_count),  # Kumulierte Summe von FDEADD_count
       survival_rate = 1 - FDEADD_count_cumsum / treatment_count 
     ) |> 
-    ungroup()  
-  
-  
-  ggplot(d, aes(x = FDEADD, y = survival_rate, colour = treatment)) +
+    ungroup() |>   
+  ggplot(aes(x = FDEADD, y = survival_rate, colour = treatment)) +
     geom_line(size = 1) +
-    labs(x = "Tage seit Randomisierung", y = "Überlebesrate")
+    labs(x = "Tage seit Randomisierung", y = "Überlebesrate") +
+    scale_y_continuous(labels = scales :: percent, expand = expansion(mult = c(0, 0.05))) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(face = "bold", size = 19, hjust = 0.5),
+      axis.title = element_text(face = "bold", size = 15), 
+      axis.text.x = element_text(size = 15),
+      axis.text.y = element_text(size = 14),
+    ) +
+    scale_color_viridis_d(option = "plasma")
 }
