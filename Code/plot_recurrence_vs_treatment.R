a3bfeaead2cc3677246e024ab693aa594e468c4d
Hanan Loulou <H.Loulou@campus.lmu.de>
2025-03-14 12:19:35 +0100

    Relative Häufigkeit anstelle von absoluten Häufigkeit

--- Code/plot_recurrence_vs_treatment.R
+++ Code/plot_recurrence_vs_treatment.R
@@ -4,35 +4,37 @@ plot_recurrence_vs_treatment <- function(data = cleaned) {
     filter(!is.na(stroke.type)) |>
     filter(!is.na(recurring.stroke.type)) |> 
     filter(!is.na(treatment))
-
-    ggplot(filtered, aes(x = treatment, fill = recurring.stroke.type)) +
-    geom_bar(position = "dodge", width = 0.8, alpha = 0.85) +
+  
+  ggplot(filtered, aes(x = treatment, fill = recurring.stroke.type)) +
+    geom_bar(position = "fill", width = 0.8, alpha = 0.85) +  
+    geom_text(stat = "count", 
+              aes(label = sprintf("%.1f%%", ..count.. / tapply(..count.., ..x.., sum)[..x..] * 100)),
+              position = position_fill(vjust = 0.5),  
+              size = 4.2) +  
     labs(
       title = "Typen von wiederkehrenden Schlaganfällen je nach Therapie",
       x = "Medikation",
-      y = "Anzahl der Patienten",
+      y = "Anteil", 
       fill = "Schlaganfalltyp"
     ) +
-      scale_fill_manual(
-        values = c("#472d7b", "#218f8d", "#90d743"),
-        labels = c("Haemorrhagic Stroke" = "Hämorrhagisch", 
-                   "Ischaemic Stroke" = "Ischämisch", 
-                   "Unknown Stroke" = "Unbestimmt")
-      ) +
+    scale_fill_manual(
+      name = "Schlaganfalltyp",
+      values = c("#472d7b", "#218f8d", "#90d743"),
+      labels = c("Haemorrhagic Stroke" = "Hämorrhagisch", 
+                 "Ischaemic Stroke" = "Ischämisch", 
+                 "Unknown Stroke" = "Unbestimmt")
+    ) +
     scale_x_discrete(labels = c("No Treatment" = "Keine Medikation")) +
-    scale_y_continuous(
-        breaks = seq(0, 175, by = 25),  
-        labels = ifelse(seq(0, 175, by = 25) %% 25 == 0, seq(0, 175, by = 25), ""),
-        expand = expansion(mult = c(0, 0.05))  
-      ) +
+    scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, 0.05))) + 
+    coord_flip() +
     theme_minimal() + 
     theme(
       plot.title = element_text(face = "bold", size = 19, hjust = 0.5),
       axis.title = element_text(face = "bold", size = 15), 
       axis.text.x = element_text(size = 14),
       axis.text.y = element_text(size = 14),
-      panel.grid.major.x = element_blank(),  
-      panel.grid.minor.x = element_blank(), 
+      panel.grid.major.y = element_blank(),  
+      panel.grid.minor.y = element_blank(), 
       legend.text = element_text(size = 10),
       legend.title = element_text(size = 12)
     )
